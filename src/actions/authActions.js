97a8b30fa092c36b913da34ad4be9e6a99efda01
snazarkoo <snazarkoo@gmail.com>
2016-10-19 01:06:42 +0300

    Added auth actions

---
+++ src/actions/authActions.js
@@ -0,0 +1,117 @@
+// The middleware to call the API for quotes
+// import { CALL_API } from './middleware/api'
+import * as types from './actionTypes';
+
+function requestLogin(creds) {
+  return {
+    type: types.LOGIN_REQUEST,
+    isFetching: true,
+    isAuthenticated: false,
+    creds
+  }
+}
+
+function receiveLogin(user) {
+  return {
+    type: types.LOGIN_SUCCESS,
+    isFetching: false,
+    isAuthenticated: true,
+    id_token: user.id_token
+  };
+}
+
+function loginError(message) {
+  return {
+    type: types.LOGIN_FAILURE,
+    isFetching: false,
+    isAuthenticated: false,
+    message
+  }
+}
+
+function requestLogout() {
+  return {
+    type: types.LOGOUT_REQUEST,
+    isFetching: true,
+    isAuthenticated: true
+  }
+}
+
+function receiveLogout() {
+  return {
+    type: types.LOGOUT_SUCCESS,
+    isFetching: false,
+    isAuthenticated: false
+  }
+}
+
+// Calls the API to get a token and
+// dispatches actions along the way
+export function loginUser(creds) {
+  
+  let config = {
+    method: 'POST',
+    headers: { 'Content-Type':'application/x-www-form-urlencoded' },
+    body: `username=${creds.username}&password=${creds.password}`
+  }
+  
+  return dispatch => {
+    // We dispatch requestLogin to kickoff the call to the API
+    dispatch(requestLogin(creds))
+    return fetch('http://localhost:3001/sessions/create', config)
+      .then(response =>
+        response.json()
+        .then(user => ({ user, response }))
+      ).then(({ user, response }) =>  {
+        if (!response.ok) {
+          // If there was a problem, we want to
+          // dispatch the error condition
+          dispatch(loginError(user.message))
+          return Promise.reject(user)
+        }
+        else {
+          // If login was successful, set the token in local storage
+          localStorage.setItem('id_token', user.id_token)
+          
+          // Dispatch the success action
+          dispatch(receiveLogin(user))
+        }
+      }).catch(err => console.log("Error: ", err))
+  }
+}
+
+// Logs the user out
+export function logoutUser() {
+  return dispatch => {
+    dispatch(requestLogout())
+    localStorage.removeItem('id_token')
+    dispatch(receiveLogout())
+  }
+}
+
+// export const QUOTE_REQUEST = 'QUOTE_REQUEST'
+// export const QUOTE_SUCCESS = 'QUOTE_SUCCESS'
+// export const QUOTE_FAILURE = 'QUOTE_FAILURE'
+
+// // Uses the API middlware to get a quote
+// export function fetchQuote() {
+//   return {
+//     [CALL_API]: {
+//       endpoint: 'random-quote',
+//       types: [QUOTE_REQUEST, QUOTE_SUCCESS, QUOTE_FAILURE]
+//     }
+//   }
+// }
+
+// // Same API middlware is used to get a 
+// // secret quote, but we set authenticated
+// // to true so that the auth header is sent
+// export function fetchSecretQuote() {
+//   return {
+//     [CALL_API]: {
+//       endpoint: 'protected/random-quote',
+//       authenticated: true,
+//       types: [QUOTE_REQUEST, QUOTE_SUCCESS, QUOTE_FAILURE]
+//     }
+//   }
+// }
